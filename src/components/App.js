6134c3eee48260ddec35a6cf3bf9d0c3f72f6cc1
Abhishek kumar <109082907+Abhishek6388@users.noreply.github.com>
2023-04-05 17:46:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,76 +1,61 @@
-
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useState,useEffect } from "react";
 import "../styles/App.css";
-// import "./styles.css"
 
 var colors = [
-  '#16a085',
-  '#27ae60',
-  '#2c3e50',
-  '#f39c12',
-  '#e74c3c',
-  '#9b59b6',
-  '#FB6964',
-  '#342224',
-  '#472E32',
-  '#BDBB99',
-  '#77B1A9',
-  '#73A857'
+  "#16a085",
+  "#27ae60",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857"
 ];
 
 const App = () => {
-
-  const [getData,setData] = useState({
-    quote:'',
-    author:''
-  })
-  const [count,setCount] = useState(0)
-
-  const [getNext,setNext] = useState(0)
-
-  const fetchData = ()  =>{
-    fetch('https://api.quotable.io/random')
-    .then((r)=>{
-      r.json().then((res)=>{
-        console.log(res);
-        setData({
-          author:res.author,
-          quote:res.content,
-        })
-      })
-    })
-    .catch((error)=>{
-      console.log(error);
-    })
-  }
-
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+  
   useEffect(()=>{
-    fetchData()
-  },[getNext])
+  fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setText(data.content);
+        setAuthor(data.author);})}
+  ,[])
 
-  const clickHandler = () =>{
-    setNext(getNext + 1)
-    if(count<10){
-      setCount(count+1)
-    }
-    else{
-      setCount(0)
-    }
-  }
 
-    return (
-      <div id="main">
-        <div id="wrapper">
-          <div className='quote-text' style={{color:colors[count]}} >
-            {getData.quote}
+  const fetchQ = () => {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setText(data.content);
+        setAuthor(data.author);
+        document.getElementsByTagName("body")[0].style.backgroundColor =
+          colors[parseInt(Math.random() * 10)];
+      });
+  };
+
+  return (
+    <div id="main">
+      <div id="wrapper">
+        <div id="quote-box">
+          <div className="quote-text">{text}</div>
+          <div className="quote-author">{author}</div>
+          <div className="buttons">
+            <button className="button" id="new-quote" onClick={fetchQ}>
+              Click
+            </button>
           </div>
-          <div className='quote-author' style={{color:colors[count]}}>
-            {getData.author}
-          </div>
-          <button onClick={clickHandler} id='new-quote'>next quote</button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default App;
